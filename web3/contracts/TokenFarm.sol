abff5bfd8e86bf7f36fe519ba13db11dd8f5ee44
Sasanga Edirisinghe <madhumaledirisinghe@gmail.com>
2022-06-08 21:08:36 +0530

    withdraw

--- web3/contracts/TokenFarm.sol
+++ web3/contracts/TokenFarm.sol
@@ -13,13 +13,21 @@ contract TokenFarm {
     mapping(address => uint256) public stakingBalance;
     mapping(address => bool) public hasStaked;
     mapping(address => bool) public isStaking;
+    address public owner;
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "caller must be the owner");
+        _;
+    }
 
     constructor(DappToken _dappToken, DaiToken _daiToken) {
         dappToken = _dappToken;
         daiToken = _daiToken;
+        owner = msg.sender;
     }
 
     function stakeTokens(uint256 _amount) public {
+        require(_amount > 0, "cannot stake 0 tokens");
         daiToken.transferFrom(msg.sender, address(this), _amount);
         stakingBalance[msg.sender] += _amount;
 
@@ -31,7 +39,7 @@ contract TokenFarm {
         hasStaked[msg.sender] = true;
     }
 
-    function issueToken() public {
+    function issueToken() public onlyOwner {
         for (uint256 i = 0; i < stakers.length; i++) {
             address recipient = stakers[i];
             uint256 balance = stakingBalance[recipient];
@@ -39,5 +47,9 @@ contract TokenFarm {
                 dappToken.transfer(recipient, balance);
             }
         }
+    }
+
+    function unstake() {
+        // withdraw all the funds
     }
 }
